65cf770e4beb8adb520eb5455f93c5e282820aca
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-10-08 17:11:49 -0300

    fix: routes in App

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,22 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
+import StudentsView from '../views/StudentsView.vue';
+import CreateStudentsView from '../views/CreateStudentsView.vue';
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'StudentsView',
+    component: StudentsView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/create',
+    name: 'CreateStudentsView',
+    component: CreateStudentsView
   }
 ]
 
