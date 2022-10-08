faa6e6ebcaa2234ad4d04a9e6502a4e632f205d0
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-10-08 01:07:02 -0300

    fix: add configs biulder in Program.cs

--- back-end/back-end.Web/Program.cs
+++ back-end/back-end.Web/Program.cs
@@ -1,11 +1,23 @@
 using students_db.Repository;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// builder.Services.AddScoped<StudentsRepository>();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StudentsContext>(options => 
+{
+    options.UseMySql(
+                @"Server=127.0.0.1; Database=students_db; Uid=root; Pwd=123456;",
+                new MySqlServerVersion(new Version(8, 0, 11))
+                // new MySqlServerVersion(new Version(8, 0, 11))
+                // Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.25-mysql")
+            );
+});
+builder.Services.AddScoped<StudentsRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,8 +31,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-StudentsContext context = new();
-context.Database.EnsureCreated();
+using(var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<StudentsContext>();
+    context.Database.EnsureDeleted();
+    context.Database.EnsureCreated();  
+    // use context
+}
+
+// StudentsContext context = new();
+
+// context.Database.EnsureDeleted();
+// context.Database.EnsureCreated();
 
 app.UseHttpsRedirection();
 
