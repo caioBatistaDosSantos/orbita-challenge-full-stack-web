e411485fad6a752ef716062da6eeacbc60f1b7fa
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-10-08 17:11:17 -0300

    feat: add view students

---
+++ front-end/src/views/StudentsView.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <SidebarComponent titleView="Consulta de Alunos"/>
+    <v-main>
+      <p>Consulta de Alunos</p>
+    </v-main>
+</v-app>
+</template>
+
+<script>
+  import SidebarComponent from '../components/SidebarComponent.vue'
+
+  export default {
+    components: {
+      SidebarComponent,
+    },
+    mounted() {
+      console.log(`the component is now mounted.`)
+  }
+}
+</script>
