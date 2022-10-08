b74679fe638444292d45122c2a166929b6c64fda
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-10-08 20:17:47 -0300

    feat: funct get and create table in StudentsView

--- front-end/src/views/StudentsView.vue
+++ front-end/src/views/StudentsView.vue
@@ -1,21 +1,62 @@
 <template>
-  <v-app>
+  <div>
+      <v-data-table
+        :headers="headers"
+        :items="students"
+        :items-per-page="5"
+        class="elevation-1"
+        :search="search"
+      ></v-data-table>
     <SidebarComponent titleView="Consulta de Alunos"/>
-    <v-main>
-      <p>Consulta de Alunos</p>
-    </v-main>
-</v-app>
+    </div>
 </template>
 
 <script>
   import SidebarComponent from '../components/SidebarComponent.vue'
+  import { GetAll } from '../services/requestApi'
 
   export default {
     components: {
       SidebarComponent,
     },
-    mounted() {
-      console.log(`the component is now mounted.`)
+    async mounted() {
+      try {
+        const students = await GetAll();
+        console.log(students)
+        var data = [];
+        students.forEach(e => {
+          data.push(
+            {
+              ...e,
+              actions: 'editar/excluir',
+            }
+          )
+        });
+
+        console.log(data)
+        this.students = data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    data () {
+      return {
+        search: '',
+        headers: [
+          {
+            text: 'Registro Acadêmico',
+            align: 'start',
+            sortable: false,
+            class: 'darken',
+            value: 'ra',
+          },
+          { text: 'Nome', value: 'name' },
+          { text: 'Email', value: 'email' },
+          { text: 'CPF', value: 'cpf' },
+          { text: 'Ações', value: 'actions' },
+        ],
+        students: [],
+      }
+    },
   }
-}
 </script>
