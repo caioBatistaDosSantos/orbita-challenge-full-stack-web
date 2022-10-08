71f0a470a4b62c47151fc752c35bcb5c08eff42d
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-10-08 18:31:07 -0300

    fix: string conecction api

--- front-end/src/services/requestApi.js
+++ front-end/src/services/requestApi.js
@@ -1,25 +1,25 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://localhost:3001',
 });
 
 export const GetAll = async () => {
-  const { data } = await api.get('/students');
+  const {data} = await api.get('/Students');
   return data;
 }
 
 export const Post = async (body) => {
-  const { data } = await api.post('/students', body);
+  const { data } = await api.post('/Students', body);
   return data;
 }
 
 export const Put = async (ra, body) => {
-  const { data } = await api.put(`/students/${ra}`, body);
+  const { data } = await api.put(`/Students/${ra}`, body);
   return data;
 }
 
 export const Delete = async (ra) => {
-  const { data } = await api.delete(`/students/${ra}`);
+  const { data } = await api.delete(`/Students/${ra}`);
   return data;
 }
