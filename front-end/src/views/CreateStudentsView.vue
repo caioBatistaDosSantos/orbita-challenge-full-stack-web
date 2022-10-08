d9515e024815a97fa173b5c2f85b27bb3a8a93f9
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-10-08 17:11:28 -0300

    feat: add view create-students

---
+++ front-end/src/views/CreateStudentsView.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <SidebarComponent titleView="Cadastrar Aluno"/>
+    <v-main>
+      <p>Cadastrar Aluno</p>
+    </v-main>
+</v-app>
+</template>
+
+<script>
+  import SidebarComponent from '../components/SidebarComponent.vue'
+
+  export default {
+    components: {
+      SidebarComponent,
+    },
+    mounted() {
+      console.log(`the component is now mounted.`)
+  }
+}
+</script>
