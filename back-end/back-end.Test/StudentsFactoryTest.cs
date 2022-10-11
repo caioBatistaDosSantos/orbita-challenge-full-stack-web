de5a42a9b5107c249d9436bb8c64617c709953cb
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-10-11 01:19:10 -0300

    test: factory class Program for test

---
+++ back-end/back-end.Test/StudentsFactoryTest.cs
@@ -0,0 +1,28 @@
+using students_db.Repository;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace students_db.Tests;
+
+public class StudentsFactoryTest : WebApplicationFactory<Program>
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        var root = new InMemoryDatabaseRoot();
+
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<StudentsContext>));
+
+            services.AddDbContext<StudentsContext>(options =>
+                options.UseInMemoryDatabase("StudentsDatabase", root));
+        });
+
+        return base.CreateHost(builder);
+    }
+}
